6fbc81bc08e6e3414a6664c70d4e451618199e75
vondrejVSB <46302578+vondrejVSB@users.noreply.github.com>
2020-05-14 19:23:30 +0200

    Update ImageConvolution.java

--- src/main/java/com/it4i/imagej/ImageConvolution.java
+++ src/main/java/com/it4i/imagej/ImageConvolution.java
@@ -1,11 +1,4 @@
 package com.it4i.imagej;
-/*
- * To the extent possible under law, the ImageJ developers have waived
- * all copyright and related or neighboring rights to this tutorial code.
- *
- * See the CC0 1.0 Universal license for details:
- *     http://creativecommons.org/publicdomain/zero/1.0/
- */
 
 import java.io.File;
 import java.io.FileInputStream;
